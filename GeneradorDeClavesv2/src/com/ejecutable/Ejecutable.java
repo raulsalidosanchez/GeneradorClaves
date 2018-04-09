dc4b9dc053a686c70f13b7f1e7ffd78422e6506e
rsalidosanchez <rsalidosanchez@ITEM-66607.emea.msad.sopra>
2018-04-09 17:13:32 +0200

    ghfd

--- GeneradorDeClavesv2/src/com/ejecutable/Ejecutable.java
+++ GeneradorDeClavesv2/src/com/ejecutable/Ejecutable.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import com.ejecutable.graficos.Marco;
 
 public class Ejecutable {
-	//comentario para commit
+	//comentario para commit hgf
 	public static void main(String[] args) {
 		Marco mimarco = new Marco();
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
