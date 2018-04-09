2dd7180a9610666c9d6211c96ee45d1bcac02397
rsalidosanchez <rsalidosanchez@ITEM-66607.emea.msad.sopra>
2018-04-09 17:28:53 +0200

    commit 2

--- GeneradorDeClavesv2/src/com/ejecutable/Ejecutable.java
+++ GeneradorDeClavesv2/src/com/ejecutable/Ejecutable.java
@@ -6,6 +6,7 @@ import com.ejecutable.graficos.Marco;
 
 public class Ejecutable {
 	//comentario para commit hgf
+	//hola nuevo comentariogf
 	public static void main(String[] args) {
 		Marco mimarco = new Marco();
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
