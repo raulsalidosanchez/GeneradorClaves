42423f97ea6d766e4546cb11a0e70f27bc246b6b
rsalidosanchez <rsalidosanchez@ITEM-66607.emea.msad.sopra>
2018-04-10 10:21:38 +0200

    Quito los comentarios que habia en el archivo Ejecutable.java

--- GeneradorDeClavesv2/src/com/ejecutable/Ejecutable.java
+++ GeneradorDeClavesv2/src/com/ejecutable/Ejecutable.java
@@ -5,8 +5,6 @@ import javax.swing.JFrame;
 import com.ejecutable.graficos.Marco;
 
 public class Ejecutable {
-	//comentario para commit hgf
-	//hola nuevo comentariogf
 	public static void main(String[] args) {
 		Marco mimarco = new Marco();
 		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
